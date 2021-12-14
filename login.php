c4ad94717b99692326e6298d3a0aec6be0c500e6
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2021-12-14 12:38:08 +0530

    14-12-'21 First build

--- login.php
+++ login.php
@@ -1,10 +1,21 @@
-<html>
-    <head>
-        <title>
-            Something
-        </title>
-    </head>
-    <body>
-        <h1> My login page </h1>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Healthy Outlet</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="logo"> Healthy <span> Outlet</span><br><span class="tagline"> Give Life the Best!</span></a>
+        <ul class="navigation">
+            <li><a href="#">Groceries</a></li>
+            <li><a href="#">Dairy</a></li>
+            <li><a href="#">Provisions and Cosmetics</a></li>
+            <li><a href="#">Snacks</a></li>
+        </ul>
+    </header>
+</body>
 </html>
