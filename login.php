279f240262f00205c27cbf2851c23ae99e00271d
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2021-12-21 11:19:35 +0530

    New update, incorrect code

--- login.php
+++ login.php
@@ -1,3 +1,35 @@
+<?php
+
+$host ="localhost";
+$user = "root";
+$password = "";
+$db = "user";
+
+$data=mysqli_connect($host,$user,$password,$db);
+if($data===false) {
+    die("connection error");
+}
+
+if($_SERVER["REQUEST_METHOD"]=="POST") {
+    $username=$_POST["username"];
+    $password=$_POST["password"];
+
+    $sql="select * from login where username= '".$username."' AND password='".$password."'";
+    $result=mysqli_query($data,$sql);
+
+    $row=mysqli_fetch_array($result);
+    if($row["usertype"]=="Athmeeya") {
+        echo "Athmeeya";
+    }
+    if($row["usertype"]=="Admin") {
+        echo "Admin";
+    }
+    else {
+        echo "Username or Password incorrect!";
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,20 +49,22 @@
             <li><a href="#">Snacks</a></li>
         </ul>
     </header>
-
-    <center>
-    <div>
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" required>
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
-    </div>
-    <div>
-        <input type="submit" value="login">
-    </div>
-    </center>
-    
+    <br><br><br><br><br><br><br><br>
+    <section class="login-form">
+        <form action="#" method="POST">
+        <h2>Login Details</h2>
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username" required>
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" required>
+        </div>
+        <div>
+            <input type="submit" value="login">
+        </div>
+        </form>
+    </section>
 </body>
 </html>
