56fac57f7e679fd683fd4dd5c241cfd13ad40145
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2021-12-28 00:49:55 +0530

    New change

---
+++ New-Version/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css" media="all">
+</head>
+<body>
+    
+    <!-- Main COntainer -->
+    <div class="main_wrapper">
+
+        <!--Header -Wrapper-->
+            <div class="header_wrapper">
+                <img src="images/HO-logo.png" alt="logo" style="float:left;">
+            </div>
+        <!-- Navigation Bar starts-->
+            <div id="navbar">
+                <ul id="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">All Products</a></li>
+                    <li><a href="#">My Account</a></li>
+                    <li><a href="#">Sign Up</a></li>
+                    <li><a href="#">Shopping Cart</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+        <!-- Navigation Bar ends-->
+
+            <div class="content_wrapper">
+                <div id="left_sidebar"> left side</div>
+                <div id="right_content"> right content</div>
+            </div>
+            <div class="footer"></div>
+    </div>
+
+</body>
+</html>
--- Reference Version/login.php
+++ Reference Version/login.php
@@ -17,14 +17,14 @@
             </div>
             <form id="login" class="input-group">
             <input type="text" class="input-field" placeholder="User ID" required>
-            <input type="text" class="input-field" placeholder="Password" required> 
+            <input type="password" class="input-field" placeholder="Password" required> 
             <input type="checkbox" class="chech-box"><span>Remember Password </span>
             <button type="submit" class="submit-btn">Log In</button>
             </form>
             <form id="register" class="input-group">
             <input type="text" class="input-field" placeholder="User ID" required>
             <input type="email" class="input-field" placeholder="Email ID" required>
-            <input type="text" class="input-field" placeholder="Enter Password" required> 
+            <input type="password" class="input-field" placeholder="Enter Password" required> 
             <input type="checkbox" class="chech-box"><span>I agree to the terms and conditions</span>
             <button type="submit" class="submit-btn">Register</button>
             </form>
