2482d9bbc4887c686daa18e3f8ea4803f401dd23
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2021-12-28 09:16:48 +0530

    Added changes

--- New-Version/index.php
+++ New-Version/index.php
@@ -26,11 +26,23 @@
                     <li><a href="#">Shopping Cart</a></li>
                     <li><a href="#">Contact Us</a></li>
                 </ul>
+
+            <div id="form">
+                <form method="get" action="results.php">
+                    <input type="text" name="user_query" placeholder="Search a product"/>
+                    <input type="submit" name="Search" value="search"/>
+                </form>
+            </div>
+
             </div>
         <!-- Navigation Bar ends-->
 
             <div class="content_wrapper">
-                <div id="left_sidebar"> left side</div>
+                <div id="left_sidebar"> 
+
+                    <div id="sidebar_title">Categories</div>
+
+                </div>
                 <div id="right_content"> right content</div>
             </div>
             <div class="footer"></div>
