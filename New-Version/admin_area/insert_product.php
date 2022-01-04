43cd7a8aaca1e85e722e77bd635ceb5d28d1428f
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-01-04 12:27:20 +0530

    Added New content and tinymce

--- New-Version/admin_area/insert_product.php
+++ New-Version/admin_area/insert_product.php
@@ -8,22 +8,28 @@ include("includes/db.php")
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({selector:'textarea'});
+    </script>
 </head>
-<body>
+<body bgcolor="grey">
 
 <form method="post" action="insert_product.php" enctype="multipart/form-data">
 
-    <table width="700" align="center">
+    <table width="700" align="center" border="1" bgcolor="blue">
 
-            <tr>
-                <td><h2>Insert New Product:</h2></td>
+            <tr align="center">
+                <td colspan="2"><h2>Insert New Product:</h2></td>
             </tr>
 
             <tr>
-                <td><input type="text" name="product_title"> </td>
+                <td align="right"><b>Product Title</b></td>
+                <td><input type="text" name="product_title" size="50"> </td>
             </tr>
 
             <tr>
+            <td align="right"><b>Product Category</b></td>
                 <td>
                     <select name="product_cart">
                         <option value="">Select A Category</option>
@@ -44,6 +50,7 @@ include("includes/db.php")
             </tr>
 
             <tr>
+                <td align="right"><b>Product Brand</b></td>
                 <td>
                         <select name="product_brand">
                             <option value="">Select A Brand</option>
@@ -64,27 +71,37 @@ include("includes/db.php")
             </tr>
 
             <tr>
+                <td align="right"><b>Product Image 1</b></td>
                 <td><input type="file" name="product_img1"> </td>
             </tr>
 
             <tr>
+                <td align="right"><b>Product Image 2</b></td>
                 <td><input type="file" name="product_img2"> </td>
             </tr>
 
             <tr>
+                <td align="right"><b>Product Image 3</b></td>
                 <td><input type="file" name="product_img3"> </td>
             </tr>
 
             <tr>
+                <td align="right"><b>Product Price</b></td>
                 <td><input type="text" name="product_price"> </td>
             </tr>
 
             <tr>
-                <td><input type="text" name="product_desc"> </td>
+                <td align="right"><b>Product Description</b></td>
+                <td><textarea name="product_desc" id="" cols="35" rows="15"></textarea> </td>
             </tr>
 
             <tr>
-                <td><input type="submit" name="submit"> </td>
+                <td align="right"><b>Product Keywords</b></td>
+                <td><input type="text" name="product_keywords" size="50"> </td>
+            </tr>
+
+            <tr align="center">
+                <td colspan="2"><input type="submit" name="submit"> </td>
             </tr>
 
     </table>
