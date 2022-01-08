e06d986751c38463e084a22e9d5160431838d537
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-01-08 19:43:54 +0530

    Enhanced changes, I think

--- New-Version/index.php
+++ New-Version/index.php
@@ -59,7 +59,7 @@ include("includes/db.php");
 
                             ?>
                         </ul>
-                <div id="sidebar_title">Brands</div>
+                <div id="sidebar_title">Brands </div>
                         <ul id="cats">
                             <?php
                                 $get_bran = "select * from brands";
@@ -76,8 +76,50 @@ include("includes/db.php");
                         </ul>
                 </div>
 
-                <div id="right_content"> right content</div>
+                <div id="right_content"> 
+                    <div id="headline">
+                        <div id="headline_content">
+                            <b>Welcome Guest</b>
+                            <b style="color:yellow;">Shopping Cart:</b>
+                            <span>- Items: - Price: </span>
+                        </div>
+                    </div>
+                    
+                    <div id="product_box">
+                        <?php
+
+                                $get_products = "select * from products order by rand() LIMIT 0,6";
+
+                                $run_products = mysqli_query($con, $get_products);
+
+                                while($row_products=mysqli_fetch_array($run_products)) {
+                                    $pro_id = $row_products['product_id'];
+                                    $pro_title = $row_products['product_title'];
+                                    $pro_cat = $row_products['cat_id'];
+                                    $pro_brand = $row_products['brand_id'];
+                                    $pro_desc = $row_products['product_desc'];
+                                    $pro_price = $row_products['product_price'];
+                                    $pro_image = $row_products['product_img1'];
+
+                                    echo "
+                                    <div id='single_product'>
+                                        <h3>$pro_title</h3>
+                                        <img src='admin_area/product_images/$pro_image' width='180' height='180'/>
+                                        <p><b>Price: Rs$pro_price/kg </b></p>
+                                        <a href='details.php?pro_id=$pro_id' style='float:left'>Details</a>
+                                        <a href='index.php?add_cart=$pro_id'><button style='float:right;'>Add to Cart</button></a>
+                                    </div>
+                                    
+                                    ";
+                                }
+
+                        ?>
+                    </div>
+
+                </div>
             </div>
+            
+            
             <div class="footer">
                 <h1 style="color=white; padding-top:30px; text-align:center">&copy; 2021 - By Idiot boys</h1>
             </div>
