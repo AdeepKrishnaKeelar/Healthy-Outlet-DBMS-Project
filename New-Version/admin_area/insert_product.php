1c160d7fd5e7d1e2611353469d106acab45eaa22
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-01-08 16:10:32 +0530

    Added more stuff

--- New-Version/admin_area/insert_product.php
+++ New-Version/admin_area/insert_product.php
@@ -101,7 +101,7 @@ include("includes/db.php")
             </tr>
 
             <tr align="center">
-                <td colspan="2"><input type="submit" name="submit"> </td>
+                <td colspan="2"><input type="submit" name="insert_product" value="Insert Product"> </td>
             </tr>
 
     </table>
@@ -111,3 +111,47 @@ include("includes/db.php")
 
 </body>
 </html>
+<?php
+
+            if(isset($_POST['insert_product'])) {
+
+                //text data variables
+                $product_title = $_POST['product_title'];
+                $product_cat = $_POST['product_cart'];
+                $product_brand = $_POST['product_brand'];
+                $product_price = $_POST['product_price'];
+                $product_desc = $_POST['product_desc'];
+                $status = 'on';
+                $product_keywords = $_POST['product_keywords'];
+
+                //image names
+                $product_img1 = $_FILES['product_img1']['name'];
+                $product_img2 = $_FILES['product_img2']['name'];
+                $product_img3 = $_FILES['product_img3']['name'];
+
+                //Image temp names 
+                $temp_name1 = $_FILES['product_img1']['tmp_name'];
+                $temp_name2 = $_FILES['product_img2']['tmp_name'];
+                $temp_name3 = $_FILES['product_img3']['tmp_name'];
+
+                if($product_title=='' OR $product_cat=='' OR $product_price=='' OR $product_brand=='' OR $product_desc=='' OR $product_keywords=='' OR $product_img1=='' OR $product_img2=='' OR $product_img3=='') {
+                        echo "<script>alert('Please fill all the fields!')</script>";
+                        exit();
+                }
+                else { 
+                //uploading image to its folder
+                move_uploaded_file($temp_name1,"product_images/$product_img1");
+                move_uploaded_file($temp_name2,"product_images/$product_img2");
+                move_uploaded_file($temp_name3,"product_images/$product_img3");
+                $insert_product = "insert into products (cat_id,brand_id,date,product_title,product_img1,product_img2,product_img3,product_price,product_desc,status) values('$product_cat','$product_brand',NOW(),'$product_title','$product_img1','$product_img2','$product_img3','$product_price','$product_desc','$status')";
+
+                $run_product = mysqli_query($con, $insert_product);
+
+                if($run_product) {
+                    echo"<script>alert('Product Inserted Successfully!')</script>";
+                }
+                }
+
+            }
+
+?>
