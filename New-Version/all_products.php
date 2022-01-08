53bee794f7651909ea01ac524b924d13e950364c
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-01-09 00:38:43 +0530

    New changes

--- New-Version/all_products.php
+++ New-Version/all_products.php
@@ -69,9 +69,29 @@ include("functions/functions.php");
                     
                     <div id="product_box">    
                         <?php
-                            getPro();
-                            getCatPro();
-                            getBrandPro();
+                            $get_products = "select * from products";
+
+                            $run_products = mysqli_query($con, $get_products);
+                            while($row_products=mysqli_fetch_array($run_products)) {
+                                $pro_id = $row_products['product_id'];
+                                $pro_title = $row_products['product_title'];
+                                $pro_cat = $row_products['cat_id'];
+                                $pro_brand = $row_products['brand_id'];
+                                $pro_desc = $row_products['product_desc'];
+                                $pro_price = $row_products['product_price'];
+                                $pro_image = $row_products['product_img1'];
+                        
+                                echo "
+                                    <div id='single_product'>
+                                    <h3>$pro_title</h3>
+                                    <img src='admin_area/product_images/$pro_image' width='180' height='180'/>
+                                    <p><b>Price: Rs$pro_price/kg </b></p>
+                                    <a href='details.php?pro_id=$pro_id' style='float:left'>Details</a>
+                                    <a href='index.php?add_cart=$pro_id'><button style='float:right;'>Add to Cart</button></a>
+                                    </div>
+                                                            
+                                    ";
+                                  }
                         ?>                    
                     </div>
 
