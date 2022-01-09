229408a4d561ce688afb69a2df6e803b89d21431
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-01-09 19:56:54 +0530

    Better changes

--- New-Version/details.php
+++ New-Version/details.php
@@ -33,8 +33,9 @@ include("functions/functions.php");
 
             <div id="form">
                 <form method="get" action="results.php" enctype="multipart/form-data">
+
                     <input type="text" name="user_query" placeholder="Search a product"/>
-                    <input type="submit" name="search" value="search"/>
+                    <input type="submit" name="Search" value="search"/>
                 </form>
             </div>
 
@@ -50,7 +51,7 @@ include("functions/functions.php");
                                 getCats();
                             ?>
                         </ul>
-                <div id="sidebar_title">Brands </div>
+                <div id="sidebar_title">Brands</div>
                         <ul id="cats">
                             <?php
                                 getBrands();
@@ -69,34 +70,39 @@ include("functions/functions.php");
                     
                     <div id="product_box">    
                         <?php
-
-                            if(isset($_GET['search'])) {
+                             
+                             if(isset($_GET['pro_id'])) {  //product_id linking to a new page with details
+                            $product_id=$_GET['pro_id'];
                             
-                            $user_keyword = $_GET['user_query'];
-                            $get_products = "select * from products where product_keywords like '%$user_keyword%'";
+                            $get_products = "select * from products where product_id='$product_id'";
 
-                            $run_products = mysqli_query($con, $get_products);
-                            while($row_products=mysqli_fetch_array($run_products)) {
-                                $pro_id = $row_products['product_id'];
-                                $pro_title = $row_products['product_title'];
-                                $pro_cat = $row_products['cat_id'];
-                                $pro_brand = $row_products['brand_id'];
-                                $pro_desc = $row_products['product_desc'];
-                                $pro_price = $row_products['product_price'];
-                                $pro_image = $row_products['product_img1'];
-                        
-                                echo "
-                                    <div id='single_product'>
-                                    <h3>$pro_title</h3>
-                                    <img src='admin_area/product_images/$pro_image' width='180' height='180'/>
-                                    <p><b>Price: Rs$pro_price/kg </b></p>
-                                    <a href='details.php?pro_id=$pro_id' style='float:left'>Details</a>
-                                    <a href='index.php?add_cart=$pro_id'><button style='float:right;'>Add to Cart</button></a>
-                                    </div>
-                                                            
-                                    ";
-                                  }
-                            }
+                             $run_products = mysqli_query($db, $get_products);
+                             while($row_products=mysqli_fetch_array($run_products)) {
+                                 $pro_id = $row_products['product_id'];
+                                 $pro_title = $row_products['product_title'];
+                                 $pro_cat = $row_products['cat_id'];
+                                 $pro_brand = $row_products['brand_id'];
+                                 $pro_desc = $row_products['product_desc'];
+                                 $pro_price = $row_products['product_price'];
+                                 $pro_image1 = $row_products['product_img1'];
+                                 $pro_image2 = $row_products['product_img2'];
+                                 $pro_image3 = $row_products['product_img3'];
+                         
+                                 echo "
+                                     <div id='single_product'>
+                                     <h3>$pro_title</h3>
+                                     <img src='admin_area/product_images/$pro_image1' width='180' height='180'/>
+                                     <img src='admin_area/product_images/$pro_image2' width='250' height='250'/>
+                                     <img src='admin_area/product_images/$pro_image3' width='250' height='250'/>
+                                     <p><b>Price: Rs$pro_price/kg </b></p>
+                                     <p>$pro_desc</p> 
+                                     <a href='index.php' style='float:left'>Go Back</a>
+                                     <a href='index.php?add_cart=$pro_id'><button style='float:right;'>Add to Cart</button></a>
+                                     </div>
+                                                             
+                                     ";
+                                   }
+                                }
                         ?>                    
                     </div>
 
