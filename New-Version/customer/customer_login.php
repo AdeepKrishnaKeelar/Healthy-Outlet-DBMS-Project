49edf8bf3f344b1f5ad7d405ba32d9becd06ed87
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-01-10 23:58:22 +0530

    New changes

--- New-Version/customer/customer_login.php
+++ New-Version/customer/customer_login.php
@@ -1,3 +1,7 @@
+<?php
+@session_start();
+include("includes/db.php");
+?>
 <div>
     <form action="checkout.php" method="POST">
         <table width="800" bgcolor="#66CCCC" align="center">
@@ -23,3 +27,23 @@
     </form>
     <h2 style="float:right; padding:10px; text-decoration:none;"><a href="customer_register.php">New Customer? Register Here</a></h2>
 </div>
+
+<?php
+
+if(isset($_POST['c_login'])) {
+    $customer_email = $_POST['c_email'];
+    $customer_pass = $_POST['c_pass'];
+
+    $sel_customer = "select * from customers where customer_email='$customer_email' AND customer_pass='$customer_pass'";
+    $run_customer = mysqli_query($con, $sel_customer);
+
+    if(mysqli_num_rows($run_customer)>0) {
+        $_SESSION['customer_email']=$customer_email;
+        echo "<script>window.open('index.php','_self')</script>";
+    }
+    else {
+        echo "<script>alert('Email or Password is wrong! Try Again!')</script>";
+    }
+}
+
+?>
---
+++ New-Version/payment_options.php
@@ -0,0 +1,5 @@
+<div>
+
+<b>Pay With </b><img src="images/paypal.jfif" alt="paypal"> <b>Or <a href="order.php">Pay Offline</a></b>
+
+</div>
