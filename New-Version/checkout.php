60f0fea0edd60cf9dda61c372bfe81911eadd95a
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2022-02-01 09:41:44 +0530

    Mostly final call

--- New-Version/checkout.php
+++ New-Version/checkout.php
@@ -27,7 +27,7 @@ include_once("functions/functions.php");
                 <li><a href="index.php">Home</a></li>
                     <li><a href="all_products.php">All Products</a></li>
                     <li><a href="customer/my_account.php">My Account</a></li>
-                    <li><a href="user_register.php">Sign Up</a></li>
+                    <li><a href="customer_register.php">Sign Up</a></li>
                     <li><a href="cart.php">Shopping Cart</a></li>
                     <li><a href="contact_us.php">Contact Us</a></li>
                 </ul>
